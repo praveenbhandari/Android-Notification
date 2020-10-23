30a87f30243966ec9c23dacfb101b486a4cb7bff
Praveen Bhandari <51204455+praveenbhandari@users.noreply.github.com>
2020-10-23 13:53:08 +0530

    initial commit

---
+++ app/src/main/java/com/example/notifica/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.notifica;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationCompat.Builder;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    NotificationManager notificationManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        Button bu=findViewById(R.id.button);
+        bu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                issueNotification();
+            }
+        });
+    }
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    void makeNotificationChannel(String id, String name, int importance)
+    {
+        NotificationChannel channel = new NotificationChannel(id, name, importance);
+        channel.setShowBadge(true); // set false to disable badges, Oreo exclusive
+
+        NotificationManager notificationManager =
+                (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+
+        assert notificationManager != null;
+        notificationManager.createNotificationChannel(channel);
+    }
+    void issueNotification()
+    {
+        // make the channel.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            makeNotificationChannel("CHANNEL_1", "Example channel", NotificationManager.IMPORTANCE_DEFAULT);
+        }
+        // the check ensures that the channel will only be made
+        // if the device is running Android 8+
+
+        NotificationCompat.Builder notification =
+                new NotificationCompat.Builder(this, "CHANNEL_1");
+        // the second parameter is the channel id.
+        // it should be the same as passed to the makeNotificationChannel() method
+
+        notification
+                .setSmallIcon(R.mipmap.ic_launcher) // can use any other icon
+                .setContentTitle("TEST NOTIFICATION")
+                .setContentText("NOTIFICATION CONTENT")
+                .setNumber(3); // this shows a number in the notification dots
+
+        NotificationManager notificationManager =
+                (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+
+        assert notificationManager != null;
+        notificationManager.notify(1, notification.build());
+        // it is better to not use 0 as notification id, so used 1.
+    }
+}
